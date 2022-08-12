d39baa3efa6335c99255c112c662dc7ed2869af8
Sima2204 <vlad.simuta@gmail.com>
2022-08-12 08:51:23 +0300

    init: all files added

---
+++ js/playList.js
@@ -0,0 +1,23 @@
+const playList = [
+    {      
+      title: 'Breaking The Habit',
+      src: '../assets/sounds/Breaking The Habit.mp3',
+      duration: '03:18'
+    },  
+    {      
+      title: 'Somewhere I Belong',
+      src: '../assets/sounds/Somewhere I Belong.mp3',
+      duration: '03:45'
+    },
+    {
+      title: 'Numb',
+      src: '../assets/sounds/Numb.mp3',
+      duration: '03:06'
+    },
+    {
+      title: 'Crawling',
+      src: '../assets/sounds/Crawling.mp3',
+      duration: '03:36'
+    },
+]
+export default playList;
